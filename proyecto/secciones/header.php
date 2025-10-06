834afb8617f0b358809eeddd9f7f65446f236fba
milagrosfigueroa <milagrosfigueroa036@gmail.com>
2025-10-06 14:49:32 -0300

    efectos en logo y logout. Alineacion de iconos acciones

--- proyecto/secciones/header.php
+++ proyecto/secciones/header.php
@@ -13,10 +13,12 @@
     <section id="seccion-header">
         <header>
             <div class="logo">
-            <img src="images/iconos_barra_header/icono_GameHub.png" alt="GameHub">
-            <h1>GameHub</h1>
+                <a href="home.php">
+                    <img src="images/iconos_barra_header/icono_GameHub.png" alt="GameHub">
+                </a>
+                    <h1>GameHub</h1>
             </div>
-
+            
             <!-- Buscador -->
             <div class="buscador-wrapper">
                 <div class="buscador">
@@ -28,45 +30,45 @@
             </div>
 
             <div class="acciones">
-            <!-- Carrito -->
-            <button class="icon-btn">
-                <img src="images/iconos_barra_header/carrito-de-compras.png">
-            </button>
-
-            <!-- Perfil -->
-            <div class="menu-container">
-                <button class="icon-btn toggle-menu" data-target="menu-perfil">
-                <img src="images/iconos_barra_header/usuario.png">
+                <!-- Carrito -->
+                <button class="icon-btn">
+                    <img src="images/iconos_barra_header/carrito-de-compras.png">
                 </button>
-                <div id="menu-perfil" class="menu">
-                <img src="images/iconos_barra_header/avatar.png" alt="avatar" class="avatar">
-                <h3>NICKNAME</h3>
-                <p>Configuraciones de la cuenta</p>
-                <ul>
-                    <li class="active">Editar Nickname</li>
-                    <li class="active">Editar Avatar</li>
-                    <li class="active">Correo electrónico</li>
-                    <li class="active">Contraseña</li>
-                    <li class="active">Idioma</li>
-                </ul>
-            </div>
-        </div>
 
-            <!-- Hamburguesa -->
-            <div class="hamb-wrapper">
+                <!-- Perfil -->
                 <div class="menu-container">
-                <button class="icon-btn toggle-menu" data-target="menu-redes"><img src="images/iconos_barra_header/menu.png" alt="menu-hamb" class="hamb"></button>
-                <div id="menu-redes" class="menu">
-                    <ul>
-                        <li><a href="html/logIn.html">Log Out</a><img src="images/iconos_menu_hamb/log-out.png"></li>
-                        <li><a href="#">Facebook</a><img src="images/iconos_menu_hamb/facebook.png"></li>
-                        <li><a href="#">Instagram</a><img src="images/iconos_menu_hamb/instagram.png"></li>
-                        <li><a href="#">Youtube</a><img src="images/iconos_menu_hamb/youtube.png"></li>
-                        <li><a href="#">Twitter</a><img src="images/iconos_menu_hamb/twitter.png"></li>
-                    </ul>
+                    <button class="icon-btn toggle-menu" data-target="menu-perfil">
+                        <img src="images/iconos_barra_header/usuario.png">
+                    </button>
+                    <div id="menu-perfil" class="menu">
+                        <img src="images/iconos_barra_header/avatar.png" alt="avatar" class="avatar">
+                        <h3>NICKNAME</h3>
+                        <p>Configuraciones de la cuenta</p>
+                        <ul>
+                            <li class="active">Editar Nickname</li>
+                            <li class="active">Editar Avatar</li>
+                            <li class="active">Correo electrónico</li>
+                            <li class="active">Contraseña</li>
+                            <li class="active">Idioma</li>
+                        </ul>
+                    </div>
                 </div>
+                <!-- Hamburguesa -->
+                <div class="hamb-wrapper">
+                    <div class="menu-container">
+                        <button class="icon-btn toggle-menu" data-target="menu-redes"></button>
+                        <img src="images/iconos_barra_header/menu.png" alt="menu-hamb" class="hamb">
+                        <div id="menu-redes" class="menu">
+                            <ul>
+                                <li><a href="logIn.php">Log Out</a><img src="images/iconos_menu_hamb/log-out.png"></li>
+                                <li><a href="#">Facebook</a><img src="images/iconos_menu_hamb/facebook.png"></li>
+                                <li><a href="#">Instagram</a><img src="images/iconos_menu_hamb/instagram.png"></li>
+                                <li><a href="#">Youtube</a><img src="images/iconos_menu_hamb/youtube.png"></li>
+                                <li><a href="#">Twitter</a><img src="images/iconos_menu_hamb/twitter.png"></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </div>
         </header>
     </section>
