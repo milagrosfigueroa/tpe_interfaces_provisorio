ff68a8e85b17792e055799b0ee7b05c8cdab827e
milagrosfigueroa <milagrosfigueroa036@gmail.com>
2025-10-06 13:36:45 -0300

    cambios en barras

--- proyecto/secciones/nav_bar.php
+++ proyecto/secciones/nav_bar.php
@@ -1,18 +1,18 @@
 <section id="seccion-sidebar">
     <nav class="sidebar">
         <ul>
-        <li><a href="html/home.html"><i class="active"><img src="images/iconos_nav_bar/casa-home.png"></i><span>Home</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/corazon-favs.png"></i><span>Mis juegos</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/estrellas-new.png"></i><span>Nuevos</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/joystick-multijugador.png"></i><span>Multijugador</span></a></li>
-        <li><a href="#"><i class="active"><img src="../images/iconos_nav_bar/controles-2-players.png"></i><span>2 jugadores</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/espadas-accion.png"></i><span>Accion</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/bandera-carreras.png"></i><span>Carreras</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pikas-de-mesa.png"></i><span>De mesa</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/fantasma-terror.png"></i><span>Terror</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pelotas-deportes.png"></i><span>Deportes</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pacman-clasicos.png"></i><span>Clasicos</span></a></li>
-        <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/gorro-cocina.png"></i><span>Cocina</span></a></li>
+            <li><a href="html/home.html"><i class="active"><img src="images/iconos_nav_bar/casa-home.png"></i><span>Home</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/corazon-favs.png"></i><span>Mis juegos</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/estrellas-new.png"></i><span>Nuevos</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/joystick-multijugador.png"></i><span>Multijugador</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/controles-2-players.png"></i><span>2 jugadores</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/espadas-accion.png"></i><span>Accion</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/bandera-carreras.png"></i><span>Carreras</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pikas-de-mesa.png"></i><span>De mesa</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/fantasma-terror.png"></i><span>Terror</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pelotas-deportes.png"></i><span>Deportes</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/pacman-clasicos.png"></i><span>Clasicos</span></a></li>
+            <li><a href="#"><i class="active"><img src="images/iconos_nav_bar/gorro-cocina.png"></i><span>Cocina</span></a></li>
         </ul>
     </nav>
 </section>
